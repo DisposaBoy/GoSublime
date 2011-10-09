47e0bb2593d4dc04a0dbc702f7a316d3fe055ee9
DisposaBoy <disposaboy@dby.me>
2011-10-09 19:46:38 +0100

    add two new helper commands to gofmt the file before finally saving it

--- gscommands.py
+++ gscommands.py
@@ -2,8 +2,18 @@ import sublime, sublime_plugin
 
 class GsCommentForwardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		self.view.run_command("toggle_comment", {"block": False})
 		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
-			self.view.run_command("toggle_comment", {"block": False})
 			self.view.run_command("move", {"by": "lines", "forward": True})
-		else:
-			self.view.run_command("toggle_comment", {"block": False})
+
+class GsFmtSaveCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
+			self.view.run_command("gs_fmt")
+		sublime.set_timeout(lambda: self.view.run_command("save"), 0)
+
+class GsFmtPromptSaveAsCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
+			self.view.run_command("gs_fmt")
+		sublime.set_timeout(lambda: self.view.run_command("prompt_save_as"), 0)
