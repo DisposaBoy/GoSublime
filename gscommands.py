2313b62524078478b8efc2f13dd283906ab6904f
DisposaBoy <disposaboy@dby.me>
2012-01-18 22:03:51 +0000

    - use helper function instead of duplicating the code

--- gscommands.py
+++ gscommands.py
@@ -1,19 +1,20 @@
 import sublime, sublime_plugin
+import gscommon as gs
 
 class GsCommentForwardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.run_command("toggle_comment", {"block": False})
-		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
+		if gs.is_go_source_view(self.view):
 			self.view.run_command("move", {"by": "lines", "forward": True})
 
 class GsFmtSaveCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
+		if gs.is_go_source_view(self.view):
 			self.view.run_command("gs_fmt")
 		sublime.set_timeout(lambda: self.view.run_command("save"), 0)
 
 class GsFmtPromptSaveAsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if self.view.score_selector(self.view.sel()[0].begin(), 'source.go') > 0:
+		if gs.is_go_source_view(self.view):
 			self.view.run_command("gs_fmt")
 		sublime.set_timeout(lambda: self.view.run_command("prompt_save_as"), 0)
