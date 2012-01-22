6ba08e24966c249c3062b5529e49ea00e42a6142
DisposaBoy <disposaboy@dby.me>
2012-01-22 19:38:48 +0000

    - add command to move the cursor to a line(row) and column

--- gscommands.py
+++ gscommands.py
@@ -17,3 +17,10 @@ class GsFmtPromptSaveAsCommand(sublime_plugin.TextCommand):
 		if gs.is_go_source_view(self.view):
 			self.view.run_command("gs_fmt")
 		sublime.set_timeout(lambda: self.view.run_command("prompt_save_as"), 0)
+
+class GsGotoRowColCommand(sublime_plugin.TextCommand):
+    def run(self, edit, row, col=0):
+        pt = self.view.text_point(row, col)
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(pt))
+        self.view.show(pt)
