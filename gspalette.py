8234b4caa42436b4b9b8adf2803f3a0f348ce55d
DisposaBoy <disposaboy@dby.me>
2012-01-29 01:13:32 +0000

    - add eperimental support for MarGo - list declarations

--- gspalette.py
+++ gspalette.py
@@ -1,6 +1,12 @@
 import sublime, sublime_plugin
-import gscommon as gs
+import margo, gscommon as gs
 from os.path import dirname, relpath
+
+class Loc(object):
+	def __init__(self, fn, row, col=0):
+		self.fn = fn
+		self.row = row
+		self.col = col
 
 class GsPaletteCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -16,7 +22,10 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			errors = gs.l_errors.get(view.id(), {})
 			for k in errors:
 				er = errors[k]
-				self.add_item(["Error on line %d" % (er.row+1), er.err], self.act_goto_error, (view, er))
+				loc = Loc(view.file_name(), er.row, er.col)
+				self.add_item(["Error on line %d" % (er.row+1), er.err], self.act_jump_to, loc)
+			if gs.setting('margo_addr'):
+				self.add_item("List Declarations", self.act_list_declarations)
 			self.show()
 
 	def show(self):
@@ -25,14 +34,14 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			items = [[' ', 'GoSublime Palette']]
 			actions = {}
 			if len(self.bookmarks) > 0:
-				b = self.bookmarks[-1]
-				line = 'line %d' % (b[1] + 1)
-				if view.file_name() == b[0]:
+				loc = self.bookmarks[-1]
+				line = 'line %d' % (loc.row + 1)
+				if view.file_name() == loc.fn:
 					fn = ''
 				else:
-					fn = relpath(b[0], dirname(b[0]))
+					fn = relpath(loc.fn, dirname(loc.fn))
 					if fn.startswith('..'):
-						fn = b[0]
+						fn = loc.fn
 					fn = '%s ' % fn
 				items[0][0] = u'\u2190 Go Back (%s%s)' % (fn, line)
 				actions[0] = (self.act_jump_back, None)
@@ -53,22 +62,43 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 	def add_item(self, item, action=None, args=None):
 		self.items.append((item, action, args))
 
-	def log_bookmark(self, fn, row, col=0):
+	def log_bookmark(self, loc):
 		view = gs.active_valid_go_view(self.window)
-		if view and fn:
+		if view:
 			bks = self.bookmarks
-			if len(bks) == 0 or (bks[-1][1] != row and bks[-1][0] != view.file_name()):
-				bks.append((fn, row, col))
+			if len(bks) == 0 or (bks[-1].row != loc.row and bks[-1].fn != view.file_name()):
+				bks.append(loc)
 	
+	def goto(self, loc):
+		self.window.open_file('%s:%d:%d' % (loc.fn, loc.row+1, loc.col+1), sublime.ENCODED_POSITION)
+
 	def act_jump_back(self, _):
 		if len(self.bookmarks) > 0:
-			fn, r, c = self.bookmarks.pop()
-			self.window.open_file('%s:%d:%d' % (fn, r+1, c+1), sublime.ENCODED_POSITION)
+			self.goto(self.bookmarks.pop())
 	
-	def act_goto_error(self, ve):
-		view, er = ve
-		row, col = gs.rowcol(view)
-		if er.row != row:
-			self.log_bookmark(view.file_name(), row, col)
-		view.run_command("gs_goto_row_col", {"row": er.row, "col": er.col})
+	def act_jump_to(self, loc):
+		view = gs.active_valid_go_view(self.window)
+		if view:
+			row, col = gs.rowcol(view)
+			if loc.row != row:
+				self.log_bookmark(loc)
+			self.goto(loc)
+
+	def act_list_declarations(self, _=None):
+		view = gs.active_valid_go_view(self.window)
+		if view:
+			m = margo.request('/declarations', {
+				'filename': view.file_name(),
+				'src': view.substr(sublime.Region(0, view.size()))
+			})
+			if m:
+				if m.has_key('error'):
+					gs.notice('GsPalette', m['error'])
+				for i, v in enumerate(m.get('declarations', [])):
+					if v['name'] in ('main', 'init'):
+						continue
+					loc = Loc(v['filename'], v['line']-1, v['column']-1)
+					prefix = u' %s \u00B7   ' % gs.CLASS_PREFIXES.get(v['kind'], '')
+					self.add_item([prefix+v['name'], v['doc']], self.act_jump_to, loc)
+				self.show()
 		
