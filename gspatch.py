99dc14308bf553ae79cefb00ac24973801133f4b
DisposaBoy <disposaboy@dby.me>
2012-03-03 21:16:02 +0000

    - re-implement using Diff-Match-Patch

--- gspatch.py
+++ gspatch.py
@@ -1,16 +1,45 @@
 import sublime
+from something_borrowed.diff_match_patch.diff_match_patch import diff_match_patch
 import gscommon as gs
-from difflib import SequenceMatcher
 
 class MergeException(Exception):
 	pass
 
-def merge(view, offset, size, dst):
+def _merge(view, size, text, edit):
+	def ss(start, end):
+		return view.substr(sublime.Region(start, end))
+	dmp = diff_match_patch()
+	diffs = dmp.diff_main(ss(0, size), text)
+	dmp.diff_cleanupEfficiency(diffs)
+	i = 0
+	dirty = False
+	for d in diffs:
+		k, s = d
+		l = len(s)
+		if k == 0:
+			# match
+			l = len(s)
+			if ss(i, i+l) != s:
+				raise MergeException('mismatch', dirty)
+			i += l
+		else:
+			dirty = True
+			if k > 0:
+				# insert
+				view.insert(edit, i, s)
+				i += l
+			else:
+				# delete
+				if ss(i, i+l) != s:
+					raise MergeException('mismatch', dirty)
+				view.erase(edit, sublime.Region(i, i+l))
+
+def merge(view, size, text):
 	if size < 0:
 		size = view.size()
 	edit = view.begin_edit()
 	try:
-		_merge(view, edit, offset, size, dst)
+		_merge(view, size, text, edit)
 	except MergeException as (err, dirty):
 		def cb():
 			if dirty:
@@ -22,24 +51,3 @@ def merge(view, offset, size, dst):
 		view.end_edit(edit)
 	return ''
 
-def _merge(view, edit, offset, size, dst):
-	def src(start, end):
-		return view.substr(sublime.Region(start, end))
-	dirty = False
-	sm = SequenceMatcher(None, src(offset, offset+size), dst)
-	for tag, s1, s2, d1, d2 in sm.get_opcodes():
-		s1 += offset
-		s2 += offset
-		if tag == 'equal':
-			if src(s1, s2) != dst[d1:d2]:
-				raise MergeException('mismatch', dirty)
-		else:
-			dirty = True
-			if tag == 'replace':
-				view.replace(edit, sublime.Region(s1, s2), dst[d1:d2])
-			elif tag == 'insert':
-				view.insert(edit, s1, dst[d1:d2])
-			elif tag == 'delete':
-				view.erase(edit, sublime.Region(s1, s2))
-			else:
-				raise MergeException('unreachable', dirty)
