7fb5c8e1d66198e2190c411f9e68ce3156c5d1eb
DisposaBoy <disposaboy@dby.me>
2012-03-11 02:14:06 +0000

    - basic command interface to take control of the build system

---
+++ gsshell.py
@@ -0,0 +1,54 @@
+import sublime, sublime_plugin
+import gscommon as gs
+
+class Prompt(object):
+	def __init__(self, view):
+		self.view = view
+		self.panel = None
+		self.subcommands = [
+			'go run', 'go build', 'go clean', 'go fix',
+			'go install', 'go test', 'go fmt', 'go vet', 'go tool'
+		]
+
+	def on_done(self, s):
+		s = s.strip()
+		if s:
+			self.view.settings().set('gs_shell_last_command', s)
+		self.view.window().run_command("exec", { 'kill': True })
+		self.view.window().run_command("exec", {
+			'shell': True,
+			'cmd': s,
+			'file_regex': '^(.+):([0-9]+):(?:([0-9]+):)?\s*(.*)',
+		})
+
+	def on_change(self, s):
+		if self.panel:
+			size = self.view.size()
+			if s.endswith('\t'):
+				lc = self.view.settings().get('gs_shell_last_command', 'go ')
+				s = s.strip()
+				if s and s not in ('', 'go'):
+					l = []
+					for i in self.subcommands:
+						if i.startswith(s):
+							l.append(i)
+					if len(l) == 1:
+						s = '%s ' % l[0]
+				elif lc:
+					s = '%s ' % lc
+				edit = self.panel.begin_edit()
+				try:
+					self.panel.replace(edit, sublime.Region(0, self.panel.size()), s)
+				finally:
+					self.panel.end_edit(edit)
+
+class GsShellCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		view = gs.active_valid_go_view(self.window)
+		view = self.window.active_view()
+		if not view:
+			gs.notice("GsShell", "this not a source.go view")
+			return
+
+		p = Prompt(view)
+		p.panel = self.window.show_input_panel("GsShell", "go ", p.on_done, p.on_change, None)
