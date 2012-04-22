9332da0a7042c3b36b211de272998f84f0806dca
DisposaBoy <disposaboy@dby.me>
2012-04-22 09:01:47 +0100

    sync

--- margo.py
+++ margo.py
@@ -1,62 +1,63 @@
-import subprocess, socket, json, traceback, os
+import subprocess, httplib, urllib, json, traceback, os
 import sublime
 import gsinit, gscommon as gs
+
+class Conn(object):
+	def __init__(self):
+		self.c = None
+
+	def init(self):
+		if self.c:
+			try:
+				self.c.close()
+			except:
+				pass
+		self.c = httplib.HTTPConnection(gs.setting('margo_addr', ''))
+
+	def post(self, path, p, h):
+		if not self.c:
+			self.init()
+		try:
+			self.c.request('POST', path, p, h)
+		except:
+			self.init()
+			self.c.request('POST', path, p, h)
+		return json.loads(self.c.getresponse().read())
+
+conn = Conn()
 
 def isinst(v, base):
 	return isinstance(v, type(base))
 
-def send(sck_addr, header, body):
-	sck = socket.socket()
-	sck.connect(sck_addr)
-	sf = sck.makefile()
-	json.dump(header, sf)
-	sf.write("\r")
-	json.dump(body, sf)
-	sf.write("\n")
-	sf.flush()
-	return json.load(sf)
-
-def call(method, a, default):
-	h = {'method': method}
+def post(path, a, default):
 	resp = None
 	try:
-		if not gs.setting('margo_enabled', False):
-			return (default, '')
-
-		margo_cmd = list(gs.setting('margo_cmd', []))
-		if not margo_cmd:
-			err = 'Missing `margo_cmd`'
-			gs.notice("MarGo", err)
-			return (default, err)
-
-		margo_addr = gs.setting('margo_addr')
-		if not margo_addr:
-			err = 'Missing `margo_addr`'
-			gs.notice("MarGo", err)
-			return (default, err)
-
-		sck_addr = margo_addr.split(':')
-		if len(sck_addr) != 2:
-			err = 'Invalid `margo_addr`... must be in the format: `host:port`'
-			gs.notice("MarGo", err)
-			return (default, err)
-		sck_addr = (sck_addr[0], int(sck_addr[1]))
+		params = urllib.urlencode({ 'data': json.dumps(a) })
+		headers = {
+			"Content-type": "application/x-www-form-urlencoded",
+			"Accept": "application/json; charset=utf-8"
+		}
 
 		try:
-			resp = send(sck_addr, h, a)
-		except socket.error:
-			margo_cmd.extend(["-d", "-addr", margo_addr])
+			resp = conn.post(path, params, headers)
+		except Exception:
+			margo_cmd = list(gs.setting('margo_cmd', []))
+			if not margo_cmd:
+				err = 'Missing `margo_cmd`'
+				gs.notice("MarGo", err)
+				return (default, err)
+			margo_cmd.extend(["-d", "-addr", gs.setting('margo_addr', '')])
 			out, err = gs.runcmd(margo_cmd)
 			
 			out = out.strip()
 			if out:
-				print('MarGo: %s' % out)
+				print('MarGo: started: %s' % out)
 			
 			err = err.strip()
 			if err:
 				gs.notice('MarGo', err)
 			else:
-				resp = send(sck_addr, h, a)
+				resp = conn.post(path, params, headers)
 	except:
 		err = traceback.format_exc()
 		gs.notice("MarGo", err)
@@ -71,26 +72,20 @@ def call(method, a, default):
 			resp["error"] = "Invalid Data"
 	return (resp["data"], resp["error"])
 
-def exit():
-	call('exit', {}, None)
-
-def hello(arg=None):
-	return call('hello', arg, None)
-
 def declarations(filename, src):
-	return call('declarations', {
+	return post('/declarations', {
 		'fn': filename,
 		'src': src
 	}, [])
 
-def package_name(filename, src):
-	return call('package_name', {
+def package(filename, src):
+	return post('/package', {
 		'fn': filename,
 		'src': src
-	}, u'')
+	}, {})
 
 def imports(filename, src, import_paths, toggle):
-	return call('imports', {
+	return post('/imports', {
 		'fn': filename,
 		'src': src,
 		'env': gsinit.env,
