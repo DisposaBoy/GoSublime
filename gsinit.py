8c52d03c403f7a1154484bfe5ccbce71f8d75ada
DisposaBoy <disposaboy@dby.me>
2012-04-29 11:20:42 +0100

    - use gsq

--- gsinit.py
+++ gsinit.py
@@ -1,8 +1,8 @@
 import os
-import gscommon as gs
-import margo
+import gscommon as gs, margo, gsq
 import sublime
 
+DOMAIN = 'GoSublime'
 
 def margo_dep(try_install):
 	motd = "hello world"
@@ -11,15 +11,15 @@ def margo_dep(try_install):
 
 	att_msg = 'Attempting to install MarGo'
 	if (not 'motd' in resp or not 'actions' in resp) and try_install:
-		gs.notice('GoSublime', att_msg)
+		gs.notice(DOMAIN, att_msg)
 		def cb():
 			out, err = gs.runcmd(['go', 'get', '-u', 'github.com/DisposaBoy/MarGo'])
 			err = '%s\n%s' % (out, err)
 			err = err.strip()
 			if err:
-				gs.notice('GoSublime', err)
+				gs.notice(DOMAIN, err)
 			else:
-				gs.notice('GoSublime', '%s: done...' % att_msg)
+				gs.notice(DOMAIN, '%s: done...' % att_msg)
 			sublime.set_timeout(lambda: margo_dep(False), 0)
 		sublime.set_timeout(cb, 0)
 		return
@@ -28,16 +28,13 @@ def margo_dep(try_install):
 		err = "Invalid response when calling MarGo. Expected `%s` got `%s`" % (motd, m)
 
 	if err:
-		gs.notice('GoSublime', err)
+		gs.notice(DOMAIN, err)
 
-try:
-	env # :|
-except:
-	env = {}
+def sync_env():
 	for k, v in gs.setting('env', {}).iteritems():
 		os.environ[k] = os.path.expandvars(os.path.expanduser(v))
 
-	for i in os.environ.iteritems():
-		env[i[0]] = i[1]
+gs.settings_obj().add_on_change('env', sync_env)
 
-	sublime.set_timeout(lambda: margo_dep(False), 3)
+gsq.dispatch(sync_env)
+gsq.dispatch(lambda: margo_dep(False))
