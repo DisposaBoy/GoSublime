d13e61f66427d2994176f0daa00aef43e98a7e41
DisposaBoy <disposaboy@dby.me>
2012-05-12 13:56:28 +0100

    - add prompt to install gocode and MarGo if the commands can't be found - report if GOROOT or GOPATH are not set - run deps check only after activating a .go file

--- gsdepends.py
+++ gsdepends.py
@@ -1,10 +1,19 @@
 import gscommon as gs, gsshell, margo, gsq
-import threading, traceback, os, re
-import sublime
+import threading, traceback, os, re, subprocess
+import sublime, sublime_plugin
 
 DOMAIN = 'GsDepends'
 CHANGES_SPLIT_PAT = re.compile(r'^##', re.MULTILINE)
 CHANGES_MATCH_PAT = re.compile(r'^\s*(r[\d.]+[-]\d+)\s+(.+?)\s*$', re.DOTALL)
+GOCODE_REPO = 'github.com/nsf/gocode'
+MARGO_REPO = 'github.com/DisposaBoy/MarGo'
+
+dep_check_done = False
+
+class GsDependsOnActivated(sublime_plugin.EventListener):
+	def on_activated(self, view):
+		if not dep_check_done:
+			sublime.set_timeout(lambda: check_depends(view), 0)
 
 def split_changes(s):
 	changes = []
@@ -17,9 +26,68 @@ def split_changes(s):
 
 def hello():
 	margo.hello("hello world")
-	gs.runcmd(['gocode'])
+	subprocess.call(['gocode'])
 
-def check_depends():
+def run_go_get(view):
+	prompt = gsshell.Prompt(view)
+	prompt.on_done('go get -u -v -x %s %s' % (GOCODE_REPO, MARGO_REPO))
+
+	def f():
+		try:
+			margo.bye_ni()
+			subprocess.call(['gocode', 'close'])
+		except Exception:
+			pass
+	gsq.dispatch(f, view)
+
+def check_depends(view):
+	global dep_check_done
+	if dep_check_done:
+		return
+
+	if not view or not view.window():
+		sublime.set_timeout(lambda: check_depends(view), 1000)
+		return
+
+	if not gs.is_go_source_view(view):
+		return
+
+	dep_check_done = True
+
+	e = gs.env()
+	if not (e.get('GOROOT') and e.get('GOPATH')):
+		gs.notice(DOMAIN, "GOPATH and/or GOROOT appear to be unset")
+
+	try:
+		subprocess.call(['go', '--help'])
+	except OSError:
+		gs.notice(DOMAIN, 'The `go` command cannot be found')
+		return
+
+	missing = []
+	cmds = [
+		['gocode', '--help'],
+		['MarGo', '--help'],
+	]
+	for cmd in cmds:
+		try:
+			subprocess.call(cmd)
+		except OSError:
+			missing.append(cmd[0])
+
+	if missing:
+		def cb(i):
+			if i == 0:
+				run_go_get(view)
+		items = [[
+			'GoSublime depends on gocode and MarGo',
+			'Install %s (using `go get`)' % ', '.join(missing),
+			'gocode repo: %s' % GOCODE_REPO,
+			'MarGo repo: %s' % MARGO_REPO,
+		]]
+		view.window().show_quick_panel(items, cb)
+		return
+
 	changelog_fn = os.path.join(sublime.packages_path(), 'GoSublime', "CHANGELOG.md")
 	try:
 		with open(changelog_fn) as f:
@@ -32,8 +100,6 @@ def check_depends():
 	if changes:
 		win = sublime.active_window()
 		if win:
-			gocode_repo = 'github.com/nsf/gocode'
-			margo_repo = 'github.com/DisposaBoy/MarGo'
 			settings_fn = 'GoSublime-GsDepends.sublime-settings'
 			settings = sublime.load_settings(settings_fn)
 			new_rev = changes[0][0]
@@ -43,12 +109,10 @@ def check_depends():
 				if i == 1 or i == 2:
 					view = win.open_file(changelog_fn)
 					if i == 1:
-						prompt = gsshell.Prompt(view)
-						prompt.on_done('go get -u -v %s %s' % (gocode_repo, margo_repo))
-						gsq.dispatch(margo.bye_ni)
-						gsq.dispatch(lambda: gs.runcmd(['gocode', 'close']))
+						run_go_get(view)
 						settings.set('tracking_rev', new_rev)
 						sublime.save_settings(settings_fn)
+
 
 			if new_rev > old_rev:
 				items = [
@@ -59,8 +123,8 @@ def check_depends():
 					],
 					[
 						"Install/Update dependencies: Gocode, MarGo",
-						"go get -u %s" % gocode_repo,
-						"go get -u %s" % margo_repo,
+						"go get -u %s" % GOCODE_REPO,
+						"go get -u %s" % MARGO_REPO,
 					],
 					[
 						"View changelog",
@@ -71,5 +135,3 @@ def check_depends():
 				win.show_quick_panel(items, on_panel_close)
 				return
 	gsq.dispatch(hello)
-
-sublime.set_timeout(check_depends, 1000)
