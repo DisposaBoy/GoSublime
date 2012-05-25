a4dc548c11429a94365c315cce098aae030cc816
DisposaBoy <disposaboy@dby.me>
2012-05-25 19:50:57 +0100

    - goodbye gofmt

--- gsfmt.py
+++ gsfmt.py
@@ -1,70 +1,22 @@
 import sublime, sublime_plugin
-import gscommon as gs
-import thatcher
+import gscommon as gs, margo, gspatch
 
-class SublimeEditor(thatcher.Editor):
-	def __init__(self, view, edit):
-		self.view = view
-		self.edit = edit
-		self.dirty = False
-
-	def update_regions(self):
-		self.regions = self.view.split_by_newlines(sublime.Region(0, self.view.size()))
-
-	def match_line(self, line_index, content):
-		self.update_regions()
-		if line_index < len(self.regions):
-			return self.view.substr(self.regions[line_index]) == content
-		return False
-
-	def insert_line(self, line_index, content):
-		self.update_regions()
-		if line_index <= len(self.regions):
-			if line_index < len(self.regions):
-				pos = self.regions[line_index].begin()
-			else:
-				pos = self.view.size()
-			self.view.insert(self.edit, pos, content+'\n')
-			self.dirty = True
-			return True
-		return False
-
-	def delete_line(self, line_index):
-		self.update_regions()
-		if line_index < len(self.regions):
-			self.view.erase(self.edit, self.view.full_line(self.regions[line_index]))
-			self.dirty = True
-			return True
-		return False
+DOMAIN = 'GsFmt'
 
 class GsFmtCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		if not (gs.setting('fmt_enabled', False) is True and gs.is_go_source_view(self.view)):
 			return
 
-		region = sublime.Region(0, self.view.size())
-		src = self.view.substr(region)
+		vsize = self.view.size()
+		src = self.view.substr(sublime.Region(0, vsize))
+		if not src.strip():
+			return
 
-		args = [gs.setting("gofmt_cmd", "gofmt"), "-d"]
-		diff, err, _ = gs.runcmd(args, src)
+		src, err = margo.fmt(self.view.file_name(), src)
+		if err or not src.strip():
+			return
+
+		dirty, err = gspatch.merge(self.view, vsize, src)
 		if err:
-			fn = self.view.file_name()
-			err = err.replace('<standard input>', fn)
-			gs.notice('GsFmt', 'File %s contains errors' % fn)
-		elif diff:
-			err = ''
-			try:
-				edit = self.view.begin_edit()
-				ed = SublimeEditor(self.view, edit)
-				err = thatcher.patch(ed, diff)
-			except Exception as e:
-				err = "%s\n\n%s" % (err, e)
-			finally:
-				self.view.end_edit(edit)
-
-			if err:
-				def cb():
-					if ed.dirty:
-						self.view.run_command('undo')
-					gs.notice("GsFmt", "Could not patch the buffer: %s" % err)
-				sublime.set_timeout(cb, 0)
+			gs.notice_undo(DOMAIN, err, self.view, dirty)
