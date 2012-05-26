355e00f209521f06bd227f620a904c1a68526613
DisposaBoy <disposaboy@dby.me>
2012-05-26 11:49:52 +0100

    - reimplement gslint to use margo/lint

--- gslint.py
+++ gslint.py
@@ -1,125 +1,176 @@
+import gscommon as gs, margo
 import sublime, sublime_plugin
-from sublime import set_timeout
-import gscommon as gs
-import re, threading, subprocess, time, traceback
-from os import unlink, listdir, chdir, getcwd, devnull
-from os.path import dirname, basename, join as pathjoin
+import threading, Queue, time
 
-LEADING_COMMENTS_PAT = re.compile(r'(^(\s*//.*?[\r\n]+|\s*/\*.*?\*/)+)', re.DOTALL)
-PACKAGE_NAME_PAT = re.compile(r'package\s+(\w+)', re.UNICODE)
-LINE_INDENT_PAT = re.compile(r'[\r\n]+[ \t]+')
+DOMAIN = 'GsLint'
 
-class GsLint(sublime_plugin.EventListener):
-    def on_close(self, view):
-        if gs.is_go_source_view(view):
-            vid = view.id()
-            def cb():
-                try:
-                    del gs.l_vsyncs[vid]
-                    del gs.l_lsyncs[vid]
-                    del gs.l_errors[vid]
-                except:
-                    pass
-            set_timeout(cb, 0)
+class FileRef(object):
+	def __init__(self, view):
+		self.view = view
+		self.src = ''
+		self.tm = 0.0
+		self.state = 0
+		self.reports = {}
 
-class ErrorReport(object):
-    def __init__(self, row, col, err):
-        self.row = row
-        self.col = col
-        self.err = err
+class Report(object):
+	def __init__(self, row, col, msg):
+		self.row = row
+		self.col = col
+		self.msg = msg
 
 class GsLintThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.daemon = True
+		self.sem = threading.Semaphore()
+		self.s = set()
+		self.q = Queue.Queue()
 
-        self.daemon = True
-        self.stop_ev = threading.Event()
-        self.ready_ev = threading.Event()
-        self.sem = threading.Semaphore()
-        self.clear(True)
+	def putq(self, fn):
+		with self.sem:
+			if fn in self.s:
+				return False
+			self.s.add(fn)
+			self.q.put(fn)
+			return True
 
-    def clear(self, clear_src=False):
-        self.view_real_path = ""
-        if clear_src:
-            self.view_src = ""
-        self.view_id = False
-        self.view = None
-        self.cmd = []
+	def popq(self):
+		fn = self.q.get()
+		with self.sem:
+			self.s.discard(fn)
+		return fn
 
-    def notify(self):
-        self.ready_ev.set()
+	def run(self):
+		while True:
+			fn = self.popq()
+			fr = ref(fn, False)
+			if fr:
+				reports = {}
+				resp, _ = margo.lint(fn, fr.src)
+				for r in resp:
+					row = r.get('row', 0)
+					col = r.get('col', 0)
+					msg = r.get('msg', '')
+					if row >= 0 and msg:
+						reports[row] = Report(row, col, msg)
 
-    def stop(self):
-        self.stop_ev.set()
+				fr = ref(fn, False)
+				if fr:
+					with sem:
+						fr.state = 1
+						fr.reports = reports
+						file_refs[fn] = fr
 
-    def run(self):
-        while not self.stop_ev.is_set():
-            try:
-                self.ready_ev.wait()
-                self.ready_ev.clear()
-                self.lint()
-            except Exception:
-                gs.notice("GsLintThread: Loop", traceback.format_exc())
+def highlight(fr):
+	sel = fr.view.sel()[0].begin()
+	row, _ = fr.view.rowcol(sel)
 
-    def lint(self):
-        return
+	if fr.state == 1:
+		fr.state = 0
+		cleanup(fr.view)
 
-def describe_errors():
-    view = gs.active_valid_go_view()
-    if view:
-        sel = view.sel()[0].begin()
-        row, _ = view.rowcol(sel)
-        gs.l_lsyncs[view.id()] = row
-        er = gs.l_errors.get(view.id(), {}).get(row, ErrorReport(0, 0, ''))
-        view.set_status('GsLint', ('GsLint: ' + er.err) if er.err else '')
+		regions = []
+		flags = sublime.HIDDEN
+		for r in fr.reports.values():
+			line = fr.view.line(fr.view.text_point(r.row, 0))
+			if r.col > 0:
+				flags = sublime.DRAW_EMPTY_AS_OVERWRITE
 
-def vsync():
-    delay = 1000
-    view = gs.active_valid_go_view()
-    if view:
-        if gs.setting('gslint_enabled', False):
-            delay = 250
-            vid = view.id()
-            src = view.substr(sublime.Region(0, view.size()))
-            tm = gs.l_vsyncs.get(vid, 0.0)
-            if gs.l_lt.view_src != src:
-                with gs.l_lt.sem:
-                    gs.l_lt.view_src = src
-                gs.l_vsyncs[vid] = time.time()
-            elif tm > 0.0:
-                timeout = int(gs.setting('gslint_timeout', 500))
-                delta = int((time.time() - tm) * 1000.0)
-                if delta >= timeout:
-                    with gs.l_lt.sem:
-                        gs.l_vsyncs[vid] = 0.0
-                        gs.l_lt.view_real_path = view.file_name()
-                        gs.l_lt.view_id = vid
-                        gs.l_lt.view = view
-                        gs.l_lt.notify()
-                        gs.l_lt.cmd = gs.setting('gslint_cmd', [])
-                    return
+			pos = line.begin() + r.col
+			if pos >= line.end():
+				pos = line.end()
 
-            row, _ = view.rowcol(view.sel()[0].begin())
-            rw = gs.l_lsyncs.get(vid, -1)
-            if row != rw:
-                gs.l_lsyncs[vid] = row
-                describe_errors()
-        else:
-            delay = 5000
-            gs.l_errors = {}
-            describe_errors()
-            view.erase_regions('GsLint-errors')
-    set_timeout(vsync, delay)
+			regions.append(sublime.Region(pos, pos))
 
+		if regions:
+			fr.view.add_regions(DOMAIN, regions, 'comment', 'dot', flags)
+		else:
+			fr.view.erase_regions(DOMAIN)
+
+	msg = ''
+	reps = fr.reports.copy()
+	l = len(reps)
+	if l > 0:
+		msg = '%s (%d)' % (DOMAIN, l)
+		r = reps.get(row)
+		if r and r.msg:
+			msg = '%s: %s' % (msg, r.msg)
+
+	if fr.state != 0:
+		msg = u'\u231B %s' % msg
+
+	fr.view.set_status(DOMAIN, msg)
+
+def cleanup(view):
+	view.set_status(DOMAIN, '')
+	view.erase_regions(DOMAIN)
+
+def watch():
+	global file_refs
+	global th
+
+	view = gs.active_valid_go_view()
+
+	if gs.setting('gslint_enabled') is not True:
+		if view:
+			with sem:
+				for fn in file_refs:
+					fr = file_refs[fn]
+					cleanup(fr.view)
+				file_refs = {}
+		sublime.set_timeout(watch, 2000)
+		return
+
+	if view and not view.is_loading():
+		fn = view.file_name()
+		fr = ref(fn)
+		with sem:
+			if fr:
+				# always use the active view (e.g in split-panes)
+				fr.view = view
+				highlight(fr)
+			else:
+				fr = FileRef(view)
+
+			file_refs[fn] = fr
+			if fr.state == 0:
+				src = view.substr(sublime.Region(0, view.size()))
+				if src != fr.src:
+					fr.src = src
+					fr.tm = time.time()
+
+				if fr.tm > 0.0:
+					timeout = int(gs.setting('gslint_timeout', 500))
+					delta = int((time.time() - fr.tm) * 1000.0)
+					if delta >= timeout:
+						fr.tm = 0.0
+						fr.state = -1
+						if not th:
+							th = GsLintThread()
+							th.start()
+						th.putq(fn)
+
+	sublime.set_timeout(watch, 500)
+
+def ref(fn, validate=True):
+	with sem:
+		if validate:
+			for fn, fr in file_refs.items():
+				if not fr.view.window() or fn != fr.view.file_name():
+					del file_refs[fn]
+		return file_refs.get(fn)
+
+def delref(fn):
+	with sem:
+		if fn in file_refs:
+			del file_refs[fn]
 try:
-    # only init once
-    gs.l_lt
-except AttributeError:
-    gs.l_lt = GsLintThread()
-    gs.l_lt.start()
-    gs.l_vsyncs = {}
-    gs.l_lsyncs = {}
-    gs.l_errors = {}
+	init_once
+except:
+	init_once = True
 
-    vsync()
+	th = None
+	sem = threading.Semaphore()
+	file_refs = {}
 
+	watch()
