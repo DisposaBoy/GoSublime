59f2d949c7506afcee3c0927232bc33a13185594
DisposaBoy <disposaboy@dby.me>
2012-05-27 11:20:46 +0100

    - add support for jumping to last imported package (for easy editing)

--- gspalette.py
+++ gspalette.py
@@ -1,6 +1,10 @@
 import sublime, sublime_plugin
 import gspatch, margo, gscommon as gs, gslint
 from os.path import dirname, relpath, basename
+import re
+
+DOMAIN = 'GsPalette'
+last_import_path = {}
 
 class Loc(object):
 	def __init__(self, fn, row, col=0):
@@ -23,8 +27,10 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 
 		if palette == 'jump_back':
 			self.jump_back()
-			return
-		self.show_palette(palette, direct)
+		elif palette == 'jump_to_imports':
+			self.jump_to_imports()
+		else:
+			self.show_palette(palette, direct)
 
 	def show_palette(self, palette, direct=False):
 		view = gs.active_valid_go_view(self.window)
@@ -43,7 +49,7 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			if pcb:
 				self.last_activate_palette = palette
 			else:
-				gs.notice('GsPalette', 'Invalid palette `%s`' % palette)
+				gs.notice(DOMAIN, 'Invalid palette `%s`' % palette)
 				palette = ''
 
 		if not direct and len(self.bookmarks) > 0:
@@ -103,6 +109,33 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 	def goto(self, loc):
 		self.window.open_file('%s:%d:%d' % (loc.fn, loc.row+1, loc.col+1), sublime.ENCODED_POSITION)
 
+	def jump_to_imports(self):
+		view = gs.active_valid_go_view()
+		if not view:
+			return
+
+		last_import = last_import_path.get(view.file_name())
+		r = None
+		if last_import:
+			offset = len(last_import) + 2
+			last_import = re.escape(last_import)
+			pat = '(?s)import.*?(?:"%s"|`%s`)' % (last_import, last_import)
+			r = view.find(pat, 0)
+
+		if not r:
+			offset = 1
+			pat = '(?s)import.*?["`]'
+			r = view.find(pat, 0)
+
+		if not r:
+			gs.notice(DOMAIN, "cannot find import declarations")
+			return
+
+		pt = r.end() - offset
+		row, col = view.rowcol(pt)
+		loc = Loc(view.file_name(), row, col)
+		self.jump_to((view, loc))
+
 	def jump_back(self, _=None):
 		if len(self.bookmarks) > 0:
 			self.goto(self.bookmarks.pop())
@@ -128,7 +161,7 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			[]
 		)
 		if err:
-			gs.notice('GsPalette', err)
+			gs.notice(DOMAIN, err)
 
 		delete_imports = []
 		add_imports = []
@@ -147,7 +180,7 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 						delete_imports.append(('%sdelete: %s ( %s )' % (indent, name, path), i))
 
 			if not skipAdd:
-				add_imports.append(('%s%s' % (indent, path), {'path': path}))
+				add_imports.append(('%s%s' % (indent, path), {'path': path, 'add': True}))
 		for i in sorted(delete_imports):
 			self.add_item(i[0], self.toggle_import, (view, i[1]))
 		self.add_item(' --- ', self.show_palette, 'imports')
@@ -155,6 +188,8 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			self.add_item(i[0], self.toggle_import, (view, i[1]))
 
 	def toggle_import(self, a):
+		global last_import_path
+
 		view, decl = a
 		im, err = margo.imports(
 			view.file_name(),
@@ -163,16 +198,20 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 			[decl]
 		)
 		if err:
-			gs.notice('GsImports', err)
+			gs.notice(DOMAIN, err)
 		else:
 			src = im.get('src', '')
 			size_ref = im.get('size_ref', 0)
 			if src and size_ref > 0:
 				dirty, err = gspatch.merge(view, size_ref, src)
 				if err:
-					gs.notice_undo('GsImports', err, view, dirty)
+					gs.notice_undo(DOMAIN, err, view, dirty)
 				elif dirty:
-					gs.notice('GsImports', 'imports updated...')
+					if decl.get('add'):
+						last_import_path[view.file_name()] = decl.get('path')
+					else:
+						last_import_path[view.file_name()] = ''
+					gs.notice(DOMAIN, 'imports updated...')
 
 	def jump_to(self, a):
 		view, loc = a
