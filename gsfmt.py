5327ec9f4993fa6a576cd02ac323241fa656b4b2
DisposaBoy <disposaboy@dby.me>
2012-06-04 10:21:07 +0100

    - be more verbose

--- gsfmt.py
+++ gsfmt.py
@@ -4,19 +4,24 @@ import gscommon as gs, margo, gspatch
 DOMAIN = 'GsFmt'
 
 class GsFmtCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		if not (gs.setting('fmt_enabled', False) is True and gs.is_go_source_view(self.view)):
-			return
+	def is_enabled(self):
+		return gs.setting('fmt_enabled', False) is True and gs.is_go_source_view(self.view)
 
+	def run(self, edit):
 		vsize = self.view.size()
 		src = self.view.substr(sublime.Region(0, vsize))
 		if not src.strip():
 			return
 
 		src, err = margo.fmt(self.view.file_name(), src)
-		if err or not src.strip():
+		if not src.strip():
+			gs.notice(DOMAIN, "cannot fmt file. it appears to contain syntax errors")
+			return
+
+		if err:
+			gs.notice(DOMAIN, "cannot fmt file. error: `%s'" % err)
 			return
 
 		dirty, err = gspatch.merge(self.view, vsize, src)
 		if err:
-			gs.notice_undo(DOMAIN, err, self.view, dirty)
+			gs.notice_undo(DOMAIN, "cannot fmt file. merge failure: `%s'" % err, self.view, dirty)
