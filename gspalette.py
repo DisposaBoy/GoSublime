9b47f6680ef506b9a131a94f5587d2e23d678cb5
DisposaBoy <disposaboy@dby.me>
2012-06-08 22:24:26 +0100

    - update to new margo imports/import_paths split

--- gspalette.py
+++ gspalette.py
@@ -157,21 +157,17 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 
 	def palette_imports(self, view, direct=False):
 		indent = '' if direct else '    '
-		im, err = margo.imports(
-			view.file_name(),
-			view.substr(sublime.Region(0, view.size())),
-			True,
-			[]
-		)
+		src = view.substr(sublime.Region(0, view.size()))
+		im, err = margo.import_paths(view.file_name(), src)
 		if err:
 			gs.notice(DOMAIN, err)
+			return
 
 		delete_imports = []
 		add_imports = []
-		imports = im.get('file_imports', [])
-		for path in im.get('import_paths', []):
+		for path in im.get('paths', []):
 			skipAdd = False
-			for i in imports:
+			for i in im.get('imports', []):
 				if i.get('path') == path:
 					skipAdd = True
 					name = i.get('name', '')
@@ -186,7 +182,8 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 				add_imports.append(('%s%s' % (indent, path), {'path': path, 'add': True}))
 		for i in sorted(delete_imports):
 			self.add_item(i[0], self.toggle_import, (view, i[1]))
-		self.add_item(' --- ', self.show_palette, 'imports')
+		if len(delete_imports) > 0:
+			self.add_item(' ', self.show_palette, 'imports')
 		for i in sorted(add_imports):
 			self.add_item(i[0], self.toggle_import, (view, i[1]))
 
@@ -197,24 +194,26 @@ class GsPaletteCommand(sublime_plugin.WindowCommand):
 		im, err = margo.imports(
 			view.file_name(),
 			view.substr(sublime.Region(0, view.size())),
-			False,
 			[decl]
 		)
 		if err:
 			gs.notice(DOMAIN, err)
 		else:
 			src = im.get('src', '')
-			size_ref = im.get('size_ref', 0)
-			if src and size_ref > 0:
-				dirty, err = gspatch.merge(view, size_ref, src)
-				if err:
-					gs.notice_undo(DOMAIN, err, view, dirty)
-				elif dirty:
-					if decl.get('add'):
-						last_import_path[view.file_name()] = decl.get('path')
-					else:
-						last_import_path[view.file_name()] = ''
-					gs.notice(DOMAIN, 'imports updated...')
+			line_ref = im.get('line_ref', 0)
+			r = view.full_line(view.text_point(max(0, line_ref-1), 0))
+			if not src or line_ref < 1 or not r:
+				return
+
+			dirty, err = gspatch.merge(view, r.end(), src)
+			if err:
+				gs.notice_undo(DOMAIN, err, view, dirty)
+			elif dirty:
+				if decl.get('add'):
+					last_import_path[view.file_name()] = decl.get('path')
+				else:
+					last_import_path[view.file_name()] = ''
+				gs.notice(DOMAIN, 'imports updated...')
 
 	def jump_to(self, a):
 		view, loc = a
