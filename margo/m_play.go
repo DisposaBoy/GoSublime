b49ea51dba1e1c0e0441a9f8a472d17902a1f973
DisposaBoy <disposaboy@dby.me>
2012-10-13 00:29:48 +0100

    * impl `go run` support

--- margo/m_play.go
+++ margo/m_play.go
@@ -9,8 +9,10 @@ import (
 )
 
 type mPlay struct {
-	Dir string            `json:"dir"`
+	Dir string `json:"dir"`
+	Src string
 	Env map[string]string `json:"env"`
+	b   *Broker
 }
 
 // todo: send the client output as it comes
@@ -20,11 +22,31 @@ func (m *mPlay) Call() (interface{}, string) {
 		env = append(env, k+"="+v)
 	}
 
-	dir, err := ioutil.TempDir(m.Env["TMP"], filepath.Join("GoSublime", "play-"))
+	tmpDir := m.Env["TMP"]
+	if tmpDir == "" {
+		tmpDir = os.TempDir()
+	}
+	tmpDir = filepath.Join(tmpDir, "GoSublime", "play")
+	// if this fails then the next operation fails as well so no point in checking this
+	os.MkdirAll(tmpDir, 0755)
+
+	dir, err := ioutil.TempDir(tmpDir, "run-")
 	if err != nil {
 		return nil, err.Error()
 	}
 	defer os.RemoveAll(dir)
+
+	if m.Src != "" {
+		err = ioutil.WriteFile(filepath.Join(dir, "a.go"), []byte(m.Src), 0755)
+		if err != nil {
+			return nil, err.Error()
+		}
+		m.Dir = dir
+	}
+
+	if m.Dir == "" {
+		return nil, "missing directory"
+	}
 
 	stdErr := bytes.NewBuffer(nil)
 	stdOut := bytes.NewBuffer(nil)
@@ -59,8 +81,9 @@ func (m *mPlay) Call() (interface{}, string) {
 }
 
 func init() {
-	registry.Register("play", func(_ *Broker) Caller {
+	registry.Register("play", func(b *Broker) Caller {
 		return &mPlay{
+			b:   b,
 			Env: map[string]string{},
 		}
 	})
