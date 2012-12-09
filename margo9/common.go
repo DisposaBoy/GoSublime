9baf0948066362b4e16b7539b82cf5d20a7b44a6
DisposaBoy <disposaboy@dby.me>
2012-12-09 09:55:03 +0000

    * add fmt helpers

--- margo9/common.go
+++ margo9/common.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"os"
 	"runtime"
 )
@@ -38,4 +43,37 @@ func orString(a ...string) string {
 		}
 	}
 	return ""
+}
+
+func parseAstFile(fn string, s string, mode parser.Mode) (fset *token.FileSet, af *ast.File, err error) {
+	fset = token.NewFileSet()
+	var src interface{}
+	if s != "" {
+		src = s
+	}
+	if fn == "" {
+		fn = "<stdin>"
+	}
+	af, err = parser.ParseFile(fset, fn, src, mode)
+	return
+}
+
+func newPrinter(tabIndent bool, tabWidth int) *printer.Config {
+	mode := printer.UseSpaces
+	if tabIndent {
+		mode |= printer.TabIndent
+	}
+	return &printer.Config{
+		Mode:     mode,
+		Tabwidth: tabWidth,
+	}
+}
+
+func printSrc(fset *token.FileSet, v interface{}, tabIndent bool, tabWidth int) (src string, err error) {
+	p := newPrinter(tabIndent, tabWidth)
+	buf := &bytes.Buffer{}
+	if err = p.Fprint(buf, fset, v); err == nil {
+		src = buf.String()
+	}
+	return
 }
