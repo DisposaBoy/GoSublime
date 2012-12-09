9081b589ee360df4e07386ed452b0bb5b43e43f1
DisposaBoy <disposaboy@dby.me>
2012-12-09 09:56:31 +0000

    * impl fmt method

---
+++ margo9/m_fmt.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go/ast"
+	"go/parser"
+)
+
+type mFmt struct {
+	Fn        string
+	Src       string
+	TabIndent bool
+	TabWidth  int
+}
+
+func (m *mFmt) Call() (interface{}, string) {
+	res := M{}
+	fset, af, err := parseAstFile(m.Fn, m.Src, parser.ParseComments)
+	if err == nil {
+		ast.SortImports(fset, af)
+		res["src"], err = printSrc(fset, af, m.TabIndent, m.TabWidth)
+	}
+	return res, errStr(err)
+}
+
+func init() {
+	registry.Register("fmt", func(b *Broker) Caller {
+		return &mFmt{
+			TabIndent: true,
+			TabWidth:  8,
+		}
+	})
+}
