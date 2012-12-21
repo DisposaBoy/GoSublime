a3b01170aa143bf224d68bcb319749b20dc982fe
DisposaBoy <disposaboy@dby.me>
2012-12-21 21:36:31 +0000

    * remove functionality now handled by mg9.py

--- gsdepends.py
+++ gsdepends.py
@@ -18,27 +18,6 @@ MARGO_REPO = 'github.com/DisposaBoy/MarGo'
 
 dep_check_done = False
 
-class GsDependsOnLoad(sublime_plugin.EventListener):
-	def on_load(self, view):
-		global dep_check_done
-		sublime.set_timeout(gs.sync_settings, 0)
-		if not dep_check_done and gs.is_go_source_view(view):
-			dep_check_done = True
-			margo.dispatch(check_depends, 'checking dependencies')
-
-def split_changes(s):
-	changes = []
-	for m in CHANGES_SPLIT_PAT.split(s):
-		m = CHANGES_MATCH_PAT.match(m)
-		if m:
-			changes.append((m.group(1), m.group(2)))
-	changes.sort(reverse=True)
-	return changes
-
-def call_cmd(cmd):
-	_, _, exc = gsshell.run(cmd)
-	return not exc
-
 def do_hello():
 	global hello_sarting
 	if hello_sarting:
@@ -69,48 +48,6 @@ def hello():
 	_, err = margo.post('/', 'hello', {}, True, False)
 	if err:
 		dispatch(do_hello, 'Starting MarGo and gocode...')
-
-def check_depends():
-	changelog_fn = gs.dist_path("CHANGELOG.md")
-	try:
-		with open(changelog_fn) as f:
-			s = f.read()
-	except IOError:
-		gs.notice(DOMAIN, traceback.format_exc())
-		return
-
-	changes = split_changes(s)
-	if changes:
-		def cb():
-			aso = gs.aso()
-			old_rev = aso.get('changelog.rev', '')
-			new_rev = changes[0][0]
-			if new_rev > old_rev:
-				aso.set('changelog.rev', new_rev)
-				gs.save_aso()
-
-				new_changes = [
-					'GoSublime: Recent Updates (you may need to restart Sublime Text for changes to take effect)',
-					'------------------------------------------------------------------------------------------',
-				]
-
-				for change in changes:
-					rev, msg = change
-					if rev > old_rev:
-						new_changes.append('\n%s\n\t%s' % (rev, msg))
-					else:
-						break
-
-				new_changes.append('\nSee %s for the full CHANGELOG\n' % changelog_fn)
-				new_changes = '\n'.join(new_changes)
-				gs.show_output(DOMAIN, new_changes, print_output=False)
-		sublime.set_timeout(cb, 0)
-	else:
-		margo.call(
-			path='/',
-			args='hello',
-			message='hello MarGo'
-		)
 
 def dispatch(f, msg=''):
 	gsq.dispatch(DOMAIN, f, msg)
