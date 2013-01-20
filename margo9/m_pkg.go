f59d0f2c70f9d5fcbaea20c3d2917182203e981b
DisposaBoy <disposaboy@dby.me>
2013-01-20 17:17:32 +0000

    * port /package

---
+++ margo9/m_pkg.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go/parser"
+)
+
+type mPkg struct {
+	Fn  string
+	Src string
+}
+
+func (m *mPkg) Call() (interface{}, string) {
+	res := M{}
+	_, af, err := parseAstFile(m.Fn, m.Src, parser.PackageClauseOnly)
+	if err == nil {
+		res["name"] = af.Name.String()
+	}
+	return res, errStr(err)
+}
+
+func init() {
+	registry.Register("pkg", func(_ *Broker) Caller {
+		return &mPkg{}
+	})
+}
