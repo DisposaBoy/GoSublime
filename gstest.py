ef4015a3c5f13d0b8d38fcaa84813fbbca40dd8e
DisposaBoy <disposaboy@dby.me>
2013-01-21 20:13:08 +0000

    * use mg9.declarations

--- gstest.py
+++ gstest.py
@@ -15,56 +15,58 @@ class GsTestCommand(sublime_plugin.WindowCommand):
 		return gs.is_go_source_view(self.window.active_view())
 
 	def run(self):
+		def f(res, err):
+			if err:
+				gs.notify(DOMAIN, err)
+				return
+
+			mats = {}
+			args = {}
+			decls = res.get('file_decls', [])
+			decls.extend(res.get('pkg_decls', []))
+			for d in decls:
+				name = d['name']
+				prefix, _ =  match_prefix_name(name)
+				if prefix and d['kind'] == 'func' and d['repr'] == '':
+					mats[True] = prefix
+					args[name] = name
+
+			names = sorted(args.keys())
+			ents = ['Run all tests and examples']
+			for k in ['Test', 'Benchmark', 'Example']:
+				if mats.get(k):
+					s = 'Run %ss Only' % k
+					ents.append(s)
+					if k == 'Benchmark':
+						args[s] = ['-test.run=none', '-test.bench="%s.*"' % k]
+					else:
+						args[s] = ['-test.run="%s.*"' % k]
+
+			for k in names:
+				ents.append(k)
+				if k.startswith('Benchmark'):
+					args[k] = ['-test.run=none', '-test.bench="^%s$"' % k]
+				else:
+					args[k] = ['-test.run="^%s$"' % k]
+
+			def cb(i, win):
+				if i >= 0:
+					a = args.get(ents[i], [])
+					win.active_view().run_command('gs9o_open', {'run': gs.lst('go', 'test', a)})
+
+			gs.show_quick_panel(ents, cb)
+
 		win, view = gs.win_view(None, self.window)
 		if view is None:
 			return
 
-		mats = {}
-		args = {}
 		vfn = gs.view_fn(view)
 		src = gs.view_src(view)
 		pkg_dir = ''
 		if view.file_name():
 			pkg_dir = os.path.dirname(view.file_name())
 
-		res, err = mg9.declarations(vfn, src, pkg_dir)
-		if err:
-			gs.notify(DOMAIN, err)
-			return
-
-		decls = res.get('file_decls', [])
-		decls.extend(res.get('pkg_decls', []))
-		for d in decls:
-			name = d['name']
-			prefix, _ =  match_prefix_name(name)
-			if prefix and d['kind'] == 'func' and d['repr'] == '':
-				mats[True] = prefix
-				args[name] = name
-
-		names = sorted(args.keys())
-		ents = ['Run all tests and examples']
-		for k in ['Test', 'Benchmark', 'Example']:
-			if mats.get(k):
-				s = 'Run %ss Only' % k
-				ents.append(s)
-				if k == 'Benchmark':
-					args[s] = '-test.run=none -test.bench=%s.*' % k
-				else:
-					args[s] = '-test.run=%s.*' % k
-
-		for k in names:
-			ents.append(k)
-			if k.startswith('Benchmark'):
-				args[k] = '-test.run=none -test.bench=^%s$' % k
-			else:
-				args[k] = '-test.run=^%s$' % k
-
-		def cb(i):
-			if i >= 0:
-				s = 'go test %s' % args.get(ents[i], '')
-				win.run_command('gs_shell', {'run': s})
-
-		win.show_quick_panel(ents, cb)
+		mg9.declarations(vfn, src, pkg_dir, f)
 
 
 def match_prefix_name(s):
