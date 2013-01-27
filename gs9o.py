a3aac16b2c3e8c0bcd0178cea95870bd3aff20d6
DisposaBoy <disposaboy@dby.me>
2013-01-27 07:53:08 +0000

    * correctly handle hist indexing when there's only one command in the history

--- gs9o.py
+++ gs9o.py
@@ -238,8 +238,12 @@ class Gs9oExecCommand(sublime_plugin.TextCommand):
 				m = HIST_EXPAND_PAT.match(cmd)
 				if m:
 					pfx = m.group(1)
-					idx = int(m.group(2))
-					cmd = hist[-idx] if idx < len(hist) else ''
+					hl = len(hist)
+					idx = hl - int(m.group(2))
+					cmd = ''
+					if idx >= 0 and idx < hl:
+						cmd = hist[idx]
+
 					if pfx == '^' or not cmd:
 						view.replace(edit, line, ('%s# %s' % (ln[0], cmd)))
 						return
