6ac325530c3373815f5298981ddd66b5924c7069
DisposaBoy <disposaboy@dby.me>
2013-02-03 10:03:53 +0000

    * sched acalls: fixes/works-around race between fmt_save/9o

--- mg9.py
+++ mg9.py
@@ -328,7 +328,7 @@ def acall(method, arg, cb):
 	if not gs.checked(DOMAIN, 'launch _send'):
 		gsq.launch(DOMAIN, _send)
 
-	_send_q.put((method, arg, cb))
+	sublime.set_timeout(lambda: _send_q.put((method, arg, cb)), 0)
 
 def bcall(method, arg):
 	q = Queue.Queue()
