a37a50ee44c1300d9ba15490f3ddb8e6e5a7e6ae
DisposaBoy <disposaboy@dby.me>
2013-03-23 04:18:46 +0000

    * cleanup binaries with old MARGO_EXE format

--- gosubl/mg9.py
+++ gosubl/mg9.py
@@ -165,9 +165,10 @@ def install(aso_tokens, force_install):
 	report_x = lambda: gs.println("GoSublime: Exception while cleaning up old binaries", gs.traceback())
 	try:
 		d = gs.home_path('bin')
+		old_pat = re.compile(r'^gosublime.r\d{2}.\d{2}.\d{2}-\d+.margo.exe$')
 		for fn in os.listdir(d):
 			try:
-				if fn != about.MARGO_EXE and about.MARGO_EXE_PAT.match(fn):
+				if fn != about.MARGO_EXE and (about.MARGO_EXE_PAT.match(fn) or old_pat.match(fn)):
 					fn = os.path.join(d, fn)
 					gs.println("GoSublime: removing old binary: %s" % fn)
 					os.remove(fn)
