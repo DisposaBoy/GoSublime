dfef3b743dae49e20baf4710622b464dc8c78dff
DisposaBoy <disposaboy@dby.me>
2013-03-28 06:21:26 +0000

    * set about.VERSION and about.ANN attrs on startup

--- GoSublime.py
+++ GoSublime.py
@@ -8,11 +8,15 @@ sys.path.insert(0, dist_dir)
 
 
 def plugin_loaded():
+	from gosubl import about
 	from gosubl import gs
 	from gosubl import mg9
 
 	gs.gs_init()
 	mg9.gs_init()
+
+	gs.set_attr('about.version', about.VERSION)
+	gs.set_attr('about.ann', about.ANN)
 
 if st2:
 	sublime.set_timeout(plugin_loaded, 0)
