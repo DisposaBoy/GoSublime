7a0d2a560fe2ede73f32c856f4e0b4df985ae110
DisposaBoy <disposaboy@dby.me>
2013-03-29 10:35:33 +0000

    * notify the user of an update when the versions go out of sync * handle changelog checking

--- GoSublime.py
+++ GoSublime.py
@@ -6,7 +6,6 @@ st2 = (sys.version_info[0] == 2)
 dist_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, dist_dir)
 
-
 def plugin_loaded():
 	from gosubl import about
 	from gosubl import gs
@@ -15,8 +14,36 @@ def plugin_loaded():
 	gs.gs_init()
 	mg9.gs_init()
 
-	gs.set_attr('about.version', about.VERSION)
-	gs.set_attr('about.ann', about.ANN)
+	# we need the values in the file on-disk but we don't want any interference with the live env
+	try:
+		g = {}
+		execfile(os.path.join(dist_dir, 'gosubl', 'about.py'), g)
+		version = g.get('VERSION', '')
+		ann = g.get('ANN', '')
+		gs.set_attr('about.version', version)
+		gs.set_attr('about.ann', ann)
+
+		if about.VERSION != version:
+			gs.show_output('GoSublime-source', '\n'.join([
+				'GoSublime source has been updated.',
+				'New version: `%s`, current version: `%s`' % (version, about.VERSION),
+				'Please restart Sublime Text to complete the update.',
+			]))
+	except Exception:
+		pass
+
+	def cb():
+		aso = gs.aso()
+		old_version = aso.get('version', '')
+		old_ann = aso.get('ann', '')
+		if about.VERSION > old_version or about.ANN > old_ann:
+			aso.set('version', about.VERSION)
+			aso.set('ann', about.ANN)
+			gs.save_aso()
+			gs.focus(gs.dist_path('CHANGELOG.md'))
+
+	sublime.set_timeout(cb, 0)
+
 
 if st2:
 	sublime.set_timeout(plugin_loaded, 0)
