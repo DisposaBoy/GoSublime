f5255fec97de6b122f20d54f715f2249c8f485b6
DisposaBoy <disposaboy@dby.me>
2013-03-31 09:47:54 +0100

    * version bump: gs.py was changed

--- gosubl/about.py
+++ gosubl/about.py
@@ -2,7 +2,7 @@ import re
 import sublime
 
 ANN = 'a13.03.31-1'
-VERSION = 'r13.03.30-3'
+VERSION = 'r13.03.31-1'
 VERSION_PAT = re.compile(r'r\d{2}.\d{2}.\d{2}-\d+', re.IGNORECASE)
 PLATFORM = '%s-%s' % (sublime.platform(), sublime.arch())
 MARGO_EXE_PREFIX = 'gosublime.margo'
