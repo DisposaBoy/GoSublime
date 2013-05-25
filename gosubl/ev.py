ffc8cc82c104b747589ceb63f122b80b4e9b98d4
DisposaBoy <disposaboy@dby.me>
2013-05-25 12:37:57 +0100

    * add basic event module and class

---
+++ gosubl/ev.py
@@ -0,0 +1,33 @@
+import threading
+import traceback
+
+class Event(object):
+	def __init__(self):
+		self.lst = []
+		self.lck = threading.Lock()
+
+	def __call__(self, *args, **kwargs):
+		with self.lck:
+			l = self.lst[:]
+
+		for f in l:
+			try:
+				f(*args, **kwargs)
+			except Exception:
+				tbck.format_exc()
+
+		return self
+
+	def __iadd__(self, f):
+		with self.lck:
+			self.lst.append(f)
+
+		return self
+
+	def __isub__(self, f):
+		with self.lck:
+			self.lst.remove(f)
+
+		return self
+
+debug = Event()
