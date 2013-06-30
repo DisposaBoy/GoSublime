7bab88dc0dddad45197f0fff6eca5373eb337b0a
DisposaBoy <disposaboy@dby.me>
2013-06-30 09:27:25 +0100

    * make sure we're syncing with the actual active_view and not some widget's view

--- gsev.py
+++ gsev.py
@@ -13,7 +13,7 @@ class EV(sublime_plugin.EventListener):
 		sublime.set_timeout(lambda: do_post_save(view), 0)
 
 	def on_activated(self, view):
-		sublime.set_timeout(lambda: do_sync_active_view(view), 0)
+		sublime.set_timeout(lambda: do_sync_active_view(view.window().active_view()), 0)
 		sublime.set_timeout(lambda: do_set_gohtml_syntax(view), 0)
 
 	def on_load(self, view):
