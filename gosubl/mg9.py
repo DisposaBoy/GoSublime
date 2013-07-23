388f2c6b5204a66839f5c53ccc7a1f19b03b2c52
DisposaBoy <disposaboy@dby.me>
2013-07-23 22:14:41 +0100

    * send autoinst setting to m_imports

--- gosubl/mg9.py
+++ gosubl/mg9.py
@@ -354,6 +354,7 @@ def declarations(fn, src, pkg_dir, f):
 
 def imports(fn, src, toggle):
 	return bcall('imports', {
+		'autoinst': gs.setting('autoinst'),
 		'env': sh.env(),
 		'fn': fn or '',
 		'src': src or '',
