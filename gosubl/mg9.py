bea1c8ff6903bc0ae77eef8b5abf7406494fa2ff
DisposaBoy <disposaboy@dby.me>
2014-02-15 15:09:57 +0000

    * don't use missing setting `installsuffix`

--- gosubl/mg9.py
+++ gosubl/mg9.py
@@ -295,7 +295,6 @@ def _complete_opts(fn, src, pos, builtins):
 			'GOROOT': nv.get('GOROOT', ''),
 			'GOPATH': nv.get('GOPATH', ''),
 		},
-		'InstallSuffix': gs.setting('installsuffix'),
 	}
 
 def fmt(fn, src):
