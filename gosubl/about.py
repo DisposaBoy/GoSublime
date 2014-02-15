d38c5642db36858cce9cf68099d38bb07a444a01
DisposaBoy <disposaboy@dby.me>
2014-02-15 15:12:14 +0000

    why aren't we bumping to r14

--- gosubl/about.py
+++ gosubl/about.py
@@ -2,7 +2,7 @@ import re
 import sublime
 
 ANN = 'a13.12.26-1'
-VERSION = 'r13.12.26-1'
+VERSION = 'r13.12.26-2'
 VERSION_PAT = re.compile(r'\d{2}[.]\d{2}[.]\d{2}-\d+', re.IGNORECASE)
 DEFAULT_GO_VERSION = 'go?'
 GO_VERSION_OUTPUT_PAT = re.compile(r'go\s+version\s+(\S+(?:\s+[+]\w+|\s+\([^)]+)?)', re.IGNORECASE)
