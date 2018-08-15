7a6fe6960c22d9195fbfed9b5f43df66ac285269
DisposaBoy <disposaboy@kuroku.io>
2018-08-15 18:20:33 +0100

    ANN 18.08.15

--- gosubl/about.py
+++ gosubl/about.py
@@ -1,8 +1,8 @@
 import re
 import sublime
 
-ANN = 'a18.07.17-1'
-VERSION = 'r18.05.19-1'
+ANN = 'a18.08.15-1'
+VERSION = 'r18.08.15-1'
 VERSION_PAT = re.compile(r'\d{2}[.]\d{2}[.]\d{2}-\d+', re.IGNORECASE)
 DEFAULT_GO_VERSION = 'go?'
 GO_VERSION_OUTPUT_PAT = re.compile(r'go\s+version\s+(\S+(?:\s+[+]\w+|\s+\([^)]+)?)', re.IGNORECASE)
